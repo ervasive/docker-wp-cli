997b9104e74c7dac65531340a4e8ecf392650a73
Ruslan Vasiliev <3du.adv@gmail.com>
2016-05-12 16:53:56 +0300

    Tuned up wp-cli wrapper script

--- wp-su.sh
+++ wp-su.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
-# This is a wrapper so that wp-cli can run as the www-data user so that permissions
-# remain correct
+#! /bin/bash
+set -e
+
+# -----------------------------------------------------------------------------
+# This is a wrapper so that wp-cli can run as the www-data user so that
+# permissions remain correct
+# -----------------------------------------------------------------------------
 WP_ROOT=${WP_ROOT:=/usr/share/nginx/html}
 
-cd $WP_ROOT
-sudo -u www-data /bin/wp-cli.phar $*
+sudo -u www-data /bin/wp-cli.phar --path=$WP_ROOT --color $*
